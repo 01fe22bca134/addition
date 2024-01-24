775f370f39d98798511793d3c8ddf6f1b58c7f3e
Harsh Chinmalli <harshanilchinmalli>
2024-01-24 16:04:21 +0530

    harsh

--- calc.java
+++ calc.java
@@ -1,35 +1,61 @@
-class calc {
-    float add(float x, float y) {
-        return x + y;
+/**
+ * The calculator class performs basic arithmetic operations such as addition,
+ * subtraction, multiplication, and division on two operands.
+ */
+class calc extends child {
+
+    float addition(float operand_1, float operand_2) {
+        return (operand_1 + operand_2);
     }
 
-    float sub(float x, float y) {
-        return x - y;
+    float subtraction(float operand_1, float operand_2) {
+        return (operand_1 - operand_2);
     }
 
-    float mul(float x, float y) {
-        return x * y;
+    float multiplication(float operand_1, float operand_2) {
+        return (operand_1 * operand_2);
     }
 
-    float div(float x, float y) {
-        return x / y;
+    float division(float operand_1, float operand_2) {
+        return (operand_1 / operand_2);
+    }
+
+    float square(float operand_1) {
+        return (operand_1 * operand_1);
+    }
+
+    float cube(float operand_1) {
+        return (operand_1 * operand_1 * operand_1);
+    }
+
+    float mod(float operand_1, float operand_2) {
+        return (operand_1 % operand_2);
     }
 
     public static void main(String[] args) {
-        calc c = new calc();
-        float x = 10;
-        float y = 20;
+        calc calc = new calc();
+        float operand_1 = 10;
+        float operand_2 = 5;
 
-        float add = c.add(x, y);
-        System.out.println(add);
+        float add_result = calc.addition(operand_1, operand_2);
+        System.out.println("Addition result is: " + add_result);
 
-        float sub = c.sub(x, y);
-        System.out.println(sub);
+        float sub_result = calc.subtraction(operand_1, operand_2);
+        System.out.println("Subtraction result is: " + sub_result);
 
-        float mul = c.mul(x, y);
-        System.out.println(mul);
+        float mult_result = calc.multiplication(operand_1, operand_2);
+        System.out.println("Multiplication result is: " + mult_result);
 
-        float div = c.div(x, y);
-        System.out.println(div);
+        float div_result = calc.division(operand_1, operand_2);
+        System.out.println("Division result is: " + div_result);
+
+        float squ_result = calc.square(operand_1);
+        System.out.println("Square result is: " + squ_result);
+
+        float cub_result = calc.cube(operand_1);
+        System.out.println("Cube result is: " + cub_result);
+
+        float mod_result = calc.mod(operand_1, operand_2);
+        System.out.println("Modulus result is: " + mod_result);
     }
 }
---
+++ child.java
@@ -0,0 +1,14 @@
+
+public class child {
+    float square(float operand_1) {
+        return (operand_1 * operand_1);
+    }
+
+    float cube(float operand_1, float operand_2) {
+        return (operand_1 * operand_2);
+    }
+
+    float mod(float operand_1, float operand_2) {
+        return (operand_1 % operand_2);
+    }
+}
